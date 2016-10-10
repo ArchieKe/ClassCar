eb851b1ef667f058a34a59b2aed8d5299510a57a
ArchieKe <gertrudeochieng@gmail.com>
2016-10-10 16:13:43 +0300

    Real world implementation of OOP

---
+++ oop.py
@@ -0,0 +1,44 @@
+class Car(object):
+    def __init__(self, name):
+        self.__washCar()   
+        self.name = name
+ 
+    def __washCar(self):
+        print 'Washing the Car'
+
+    def fix(self):
+        print 'Fixing Car...'
+
+    def drive(self):             
+        pass
+ 
+    def breaking(self):             
+        pass
+ 
+""" Inheritance: The following class 'Inherits' from the Super Class """
+class Motorcycle(Car):
+    def drive(self):
+        return 'Riding Bike!'
+ 
+    def breaking(self):
+        return 'Bike stopping'
+
+""" Inheritance: The following class 'Inherits' from the Super Class """
+class Sedan(Car):
+    def drive(self):
+        return 'The Sedan is being driven'
+ 
+    def breaking(self):
+        return 'Sedan breaking!'
+
+
+""" Polymorphism is implemented below """
+cars = [Sedan('Lexus'), Sedan('Shelby'), Motorcycle('Suzuki')]
+for car in cars:
+    print car.name + ': ' + car.drive()
+
+""" The following example demonstrates Encapsulation whereby __washCar() is not accessible outside the scope of the class """
+mazda = Car('Axela')
+mazda.fix()
+#mazda.__washCar() is not accesible from object.
+
